d97736bbd1d77a9a0bb750c620d4116a70311efc
rnjstjfgud12 <61874872+rnjstjfgud12@users.noreply.github.com>
2020-04-15 17:08:56 +0900

    JS Clock
    Add analog clock

--- clock.js
+++ clock.js
@@ -1,6 +1,7 @@
 const secondsHand = document.querySelector(".second_hand");
 const minsHand = document.querySelector(".min_hand");
 const hoursHand = document.querySelector(".hour_hand");
+const clockContainer = document.querySelector(".clock"), clockTitle = clockContainer.querySelector("h1");
 
 function setDate() {
     const now = new Date();
@@ -19,4 +20,21 @@ function setDate() {
 
 }
 
+function getTime() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    clockTitle.innerText = `${
+        hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds
+    }`; // 여기서 ?는 if 역할을 하고 :는 else 역할을 한다
+}
+
 setInterval(setDate, 1000);
+
+function init() {
+    getTime();
+    setInterval(getTime, 1000);  //인터벌 반복 --> 1(1000)초마다 반복해서 값 불러옴
+}
+
+init();
