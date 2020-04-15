d76d6020ee69e4521585ed04e01e8c3b0a13fe44
rnjstjfgud12 <61874872+rnjstjfgud12@users.noreply.github.com>
2020-04-15 16:22:53 +0900

    JS Clock
    Using JS to Clock

---
+++ clock.js
@@ -0,0 +1,22 @@
+const secondsHand = document.querySelector(".second_hand");
+const minsHand = document.querySelector(".min_hand");
+const hoursHand = document.querySelector(".hour_hand");
+
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) +90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) +90;
+    hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+}
+
+setInterval(setDate, 1000);
